947d9f5885962a50dc65945fb86dcbc2222d5618
crypticwoodwhite <crypticwoodwhite@gmail.com>
2022-07-08 18:04:21 -0700

    put search bar directly in app file

--- src/App.js
+++ src/App.js
@@ -3,22 +3,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <main className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Github topic explorer
       </header>
-    </div>
+      <div className="search-bar">
+        <label for="search-bar-input">Search</label>
+        <input id="search-bar-input" type="text" placeholder="React"></input>
+      </div>
+    </main>
   );
 }
 
