0431ab2a8c4335cfa52186a0652c756246a58001
crypticwoodwhite <crypticwoodwhite@gmail.com>
2022-07-09 12:01:17 -0700

    Make things look somewhat less ugly

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,13 @@ interface ISearchResults {
 }
 
 function App() {
-  const [ search, setSearch ] = useState<string>("react");
+  const [ search, setSearch ] = useState("react");
   const [ searchResults, setSearchResults ] = useState<ISearchResults>({topic: "", stargazerCount: 0, relatedTopics: [""]});
+  const [ error, setError ] = useState<Error>();
 
   const searchTopic = (topic: string) => {
+    setError(undefined);
+
     fetch("https://api.github.com/graphql", {
       method: 'POST',
       body: JSON.stringify({
@@ -43,8 +46,7 @@ function App() {
       }
       setSearchResults(newResults);
     }).catch(err => {
-      console.log("err", err);
-      // do something with err
+      setError(err);
     })
   }
 
@@ -53,9 +55,7 @@ function App() {
   }, []);
 
   const onChangeSearch = (newVal: string) => {
-    if (newVal) {
-      setSearch(newVal.trim().toLowerCase());
-    };
+    setSearch(newVal.trim().toLowerCase());
   }
 
   const onSubmit = (ev: React.FormEvent<HTMLFormElement>) => {
@@ -69,17 +69,64 @@ function App() {
     searchTopic(topic);
   }
 
+  const renderResults = (): React.ReactNode => {
+    if (error) {
+      return (
+        <>
+          <p className="error-message">Oh no! Something broke!</p>
+          <p>{error.name}</p>
+          <p>{error.message}</p>
+        </>
+      )
+    }
+
+    return (
+      <>
+        <h2>Search results</h2>
+        <p className="search-results-subtitle">Topic:</p>
+        <p>{searchResults.topic}</p>
+        <p className="search-results-subtitle">Stargazers:</p>
+        <p>{searchResults.stargazerCount > 0 ? searchResults.stargazerCount : "No one cares :("}</p>
+        <p className="search-results-subtitle">Related topics:</p>
+        {
+          searchResults.relatedTopics.length > 0 ?
+            (
+              searchResults.relatedTopics.map((rt, i) => 
+                <button
+                  key={i}
+                  className="related-topics-btn"
+                  aria-label={`Click button to search for ${rt}`}
+                  onClick={ev => onClickRelatedTopic(ev, rt)}
+                >
+                  {rt}
+                </button>
+              )
+            ) :
+            (
+              <p>No related topics because it's so unique!</p>
+            )
+        }
+      </>
+    )
+  }
+
   return (
     <main className="App">
       <header>
         <h1>Github topic explorer</h1>
       </header>
-      <div className="search">
+      <hr />
+      <div className="search-bar">
         <form
           id="search-form"
           onSubmit={onSubmit}
         >
-          <label htmlFor="search-bar-input">Search</label>
+          <label
+            htmlFor="search-bar-input"
+            className="search-bar-label"
+          >
+            Search Github for any topic!
+          </label>
           <input
             id="search-bar-input"
             type="search"
@@ -89,25 +136,15 @@ function App() {
           />
           <button
             type="submit"
+            disabled={!!!search.length}
           >
             Search
           </button>
         </form>
       </div>
+      <hr />
       <div className="search-results">
-        <h2>Search results</h2>
-        <p>Topic: {searchResults.topic}</p>
-        <p>Stargazers: {searchResults.stargazerCount}</p>
-        <p>Related topics:</p>
-        {
-          searchResults.relatedTopics.map(rt => 
-            <button
-              onClick={ev => onClickRelatedTopic(ev, rt)}
-            >
-              {rt}
-            </button>
-          )
-        }
+        {renderResults()}
       </div>
     </main>
   );
